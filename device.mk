2350d03a44c66418617a59e46879cf3830f70421
Christian Oder <myself5@carbonrom.org>
2017-11-24 20:32:33 +0100

    wahoo: don't overwrite frameworks-res from vendor

--- device.mk
+++ device.mk
@@ -550,8 +550,6 @@ PRODUCT_PACKAGES += \
     liblzma.vndk-sp\
     libz.vndk-sp\
 
-PRODUCT_ENFORCE_RRO_TARGETS := framework-res
-
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m
