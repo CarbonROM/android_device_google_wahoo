b4bfe64c3bd2624af79be1d0596c156c79f53f69
Christian Oder <myself5@carbonrom.org>
2018-03-30 19:04:28 +0200

    wahoo: Include Ambient Music
    Change-Id: Ic2b94d74b2149bb38b23396a240c5c76c1c66656

--- device-carbon.mk
+++ device-carbon.mk
@@ -13,3 +13,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Include AmbientSense vendor if it's available
+-include vendor/ambientmusic/ambient/ambient-vendor.mk
