0913df87ce5c16651edc48dabb9f9e26100adfa1
Christian Oder <myself5@carbonrom.org>
2019-03-29 21:54:33 +0100

    Only inherit Elmyra for Pixel devices
    Change-Id: Id95c04327aa7d661e2d07bc3b31f7b8217200444

--- device-carbon.mk
+++ device-carbon.mk
@@ -12,3 +12,6 @@ PRODUCT_COPY_FILES += \
 # EUICC feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Include GoogleServices repo if available
+include external/google/GoogleServices.mk
