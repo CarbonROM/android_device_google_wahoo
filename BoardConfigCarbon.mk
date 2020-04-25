cd77283f53aa7344d8768d27b522edecdec77676
Paul Keith <javelinanddart@gmail.com>
2020-04-25 14:36:11 -0500

    wahoo: Add prebuilt dtbo images for walleye and taimen
    * These were compiled out of tree and by hand, as anything built in the
      source tree doesn't actually seem to boot on wahoo for some reason
    * Instructions to reproduce are in the README
    
    Change-Id: I0208e8900c2dd3ce2c88c61abfa909e8435c1a6f

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -8,8 +8,10 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_NEEDS_DTBOIMAGE := true
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# DTBO image
+BOARD_PREBUILT_DTBOIMAGE := device/google/wahoo/kernel/$(TARGET_DEVICE)_dtbo.img
 
 # Audio
 BOARD_SUPPORTS_SOUND_TRIGGER := true
